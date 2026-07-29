0692e0db5a232c8c71a7047b25998116f5f73880
ndaisme <136563613+ndaisme@users.noreply.github.com>
2026-07-29 12:21:52 +0000

    build.gradle.kts up

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugin {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.google.devtools.kps) apply false
+  alias(libs.plugins.roborazzi) apply false
+  alias(libs.plugins.secrets) apply false
+  alias(libs.plugins.google.services) apply false
+}
